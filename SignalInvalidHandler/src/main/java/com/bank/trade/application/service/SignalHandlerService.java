384a4d22d6e3b8d55ebb3482f98f1713a8c5bd0a
Tharindu92 <tharindudesilva92@gmail.com>
2023-04-23 01:34:38 +0200

    Implementation of controller and services in Invalid Signal Handler module

---
+++ SignalInvalidHandler/src/main/java/com/bank/trade/application/service/SignalHandlerService.java
@@ -0,0 +1,22 @@
+package com.bank.trade.application.service;
+
+import com.bank.trade.application.handler.SignalHandleServiceAbstract;
+import com.bank.trade.application.signal.Signal;
+import com.bank.trade.application.signal.SignalInvalid;
+import com.bank.trade.application.signal.SignalOne;
+import com.bank.trade.application.util.AppConstants;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SignalHandlerService extends SignalHandleServiceAbstract {
+
+    @Override
+    public String invokeTradeAlgorithm(String signalInt) {
+        Signal signalObj = getSignal(signalInt);
+        if(signalObj.getClass().getSimpleName().equals(SignalInvalid.class.getSimpleName())) {
+            return callAlgorithm(signalObj);
+        }else{
+            return AppConstants.SIGNAL_MISMATCH_ERROR_MSG;
+        }
+    }
+}
