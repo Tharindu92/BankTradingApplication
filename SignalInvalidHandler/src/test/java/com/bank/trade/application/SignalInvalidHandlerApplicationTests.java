8ce58f3c19d170af9cd9aacd4d99d3df6b81552d
Tharindu92 <tharindudesilva92@gmail.com>
2023-04-23 01:45:48 +0200

    Implementation of unit test cases for Invalid Signal Handler module

---
+++ SignalInvalidHandler/src/test/java/com/bank/trade/application/SignalInvalidHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.trade.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignalInvalidHandlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ SignalInvalidHandler/src/test/java/com/bank/trade/application/controller/SignalHandleControllerTest.java
@@ -0,0 +1,20 @@
+package com.bank.trade.application.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest
+class SignalHandleControllerTest {
+
+    @Autowired
+    SignalHandleController controller;
+
+    @Test
+    void testSignalInvalidController() {
+        ResponseEntity<String> actualResponse = controller.executeTradeAlgorithm();
+        Assertions.assertEquals(actualResponse.getStatusCode().value(),200);
+    }
+}
