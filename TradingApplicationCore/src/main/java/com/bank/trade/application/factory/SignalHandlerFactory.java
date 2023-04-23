ea80fb6ed5000cec567edb02f2fda5d3343303a5
Tharindu92 <tharindudesilva92@gmail.com>
2023-04-23 12:39:18 +0200

    Make getSignal method static

--- TradingApplicationCore/src/main/java/com/bank/trade/application/factory/SignalHandlerFactory.java
+++ TradingApplicationCore/src/main/java/com/bank/trade/application/factory/SignalHandlerFactory.java
@@ -4,22 +4,22 @@ import com.bank.trade.algo.Algo;
 import com.bank.trade.application.signal.Signal;
 import com.bank.trade.application.signal.SignalInvalid;
 import com.bank.trade.application.util.AppConstants;
+import lombok.extern.slf4j.Slf4j;
 
 import java.lang.reflect.InvocationTargetException;
 
+@Slf4j
 public class SignalHandlerFactory {
-    private Algo algo;
+    private SignalHandlerFactory(){
 
-    public SignalHandlerFactory(Algo algo){
-        this.algo = algo;
     }
 
-    public Signal getSignal(String signal){
+    public static Signal getSignal(String signal, Algo algo){
         try {
             String className = AppConstants.SIGNAL_CLASS_PATH + AppConstants.SIGNAL_CLASS_BASE_NAME + signal;
             return (Signal) Class.forName(className).getConstructor(Algo.class).newInstance(algo);
         } catch (ClassNotFoundException | InvocationTargetException | InstantiationException | IllegalAccessException | NoSuchMethodException e) {
-            System.out.println(e.getLocalizedMessage());
+            log.error("Error in getting signal from given signal " + e.getMessage());
             return new SignalInvalid(algo);
         }
     }
