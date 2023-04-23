40e96a0e084af64651e67541b595dac59aed9091
Tharindu92 <tharindudesilva92@gmail.com>
2023-04-23 04:05:07 +0200

    Change Gateway application for new design

--- TradingApplication/src/main/java/com/bank/trade/application/Application.java
+++ TradingApplication/src/main/java/com/bank/trade/application/Application.java
@@ -1,33 +1,44 @@
 package com.bank.trade.application;
 
 import com.ban.trade.handle.SignalHandler;
-import com.bank.trade.algo.Algo;
+import com.bank.trade.application.cache.SignalEndpoints;
+import com.bank.trade.application.util.CacheConstants;
+
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Locale;
+
+import static java.net.URI.create;
 
 public class Application implements SignalHandler {
     public void handleSignal(int signal) {
-        Algo algo = new Algo();
-        switch (signal) {
-            case 1:
-                algo.setUp();
-                algo.setAlgoParam(1,60);
-                algo.performCalc();
-                algo.submitToMarket();
-                break;
-            case 2:
-                algo.reverse();
-                algo.setAlgoParam(1,80);
-                algo.submitToMarket();
-                break;
-            case 3:
-                algo.setAlgoParam(1,90);
-                algo.setAlgoParam(2,15);
-                algo.performCalc();
-                algo.submitToMarket();
-                break;
-            default:
-                algo.cancelTrades();
-                break;
-        }
-        algo.doAlgo();
+        String url = getEndPointUrl(signal);
+        callRestService(url);
     }
+
+    protected String getEndPointUrl(int signal){
+        String signalStr = CacheConstants.SIGNAL_CLASS_BASE_NAME.toUpperCase(Locale.ROOT)+signal;
+        return SignalEndpoints.getEndPointsBySignal(signalStr);
+    }
+
+    protected HttpResponse<String> callRestService(String url){
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(create(url))
+                .build();
+        HttpResponse<String> httpResponse = null;
+        try {
+            httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        } catch (IOException  e) {
+            System.err.println("Error calling the micro service: " + e.getMessage());
+        } catch (InterruptedException e) {
+            System.err.println("Error calling the micro service: " + e.getMessage());
+            Thread.currentThread().interrupt();
+        }
+        return httpResponse;
+    }
+
 }
