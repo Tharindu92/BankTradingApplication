74011e14bb0f9c40ed2b64870c3a47442eae8588
Tharindu92 <tharindudesilva92@gmail.com>
2023-04-23 02:12:06 +0200

    Set up util class for constants

---
+++ TradingApplicationCore/src/main/java/com/bank/trade/application/util/AppConstants.java
@@ -0,0 +1,9 @@
+package com.bank.trade.application.util;
+
+public class AppConstants {
+    public final static String SIGNAL_CLASS_PATH = "com.bank.trade.application.signal.";
+    public final static String SIGNAL_CLASS_BASE_NAME = "Signal";
+    public final static String ALGO_INVOCATION_MESSAGE = "Algo invoked successfully";
+    public final static String SIGNAL_ONE = "One";
+    public final static String SIGNAL_MISMATCH_ERROR_MSG = "Returned Signal Class is wrong. Please check";
+}
