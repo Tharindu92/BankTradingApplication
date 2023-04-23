30261387d5114388ec0ffc1985d42b8341d472d9
Tharindu92 <tharindudesilva92@gmail.com>
2023-04-23 02:36:45 +0200

    Implementation of controller layer and service layer of Signal three module.

---
+++ SignalThreeHandler/src/main/java/com/bank/trade/application/controller/SignalHandleController.java
@@ -0,0 +1,23 @@
+package com.bank.trade.application.controller;
+
+import com.bank.trade.application.service.SignalHandlerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/trade/signals")
+public class SignalHandleController {
+
+    @Autowired
+    private SignalHandlerService signalHandlerService;
+
+    @GetMapping(value = "/three")
+    public ResponseEntity<String> executeTradeAlgorithm(){
+        signalHandlerService.invokeTradeAlgorithm("Three");
+        return new ResponseEntity<>(HttpStatus.Series.SUCCESSFUL.name(), HttpStatus.OK);
+    }
+}
