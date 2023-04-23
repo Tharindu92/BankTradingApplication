6f56eca43890480f20674ed1fb916129a4dc3373
Tharindu92 <tharindudesilva92@gmail.com>
2023-04-23 02:28:47 +0200

    Initialization of Signal Two Handler module

---
+++ SignalTwoHandler/src/main/java/com/bank/trade/application/SignalTwoHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.bank.trade.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignalTwoHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SignalTwoHandlerApplication.class, args);
+    }
+
+}
