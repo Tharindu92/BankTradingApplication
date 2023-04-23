cfeb8459b445d17dded9304f4c3961c5914762b6
Tharindu92 <tharindudesilva92@gmail.com>
2023-04-23 02:30:22 +0200

    Implementation of controller layer and service layer of Signal two module.

---
+++ SignalTwoHandler/src/main/java/com/bank/trade/application/controller/SignalHandleController.java
@@ -0,0 +1,24 @@
+package com.bank.trade.application.controller;
+
+import com.bank.trade.application.service.SignalHandlerService;
+import com.bank.trade.application.util.AppConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/trade/signals")
+public class SignalHandleController {
+
+    @Autowired
+    private SignalHandlerService signalHandlerService;
+
+    @GetMapping(value = "/two")
+    public ResponseEntity<String> executeTradeAlgorithm(){
+        signalHandlerService.invokeTradeAlgorithm(AppConstants.SIGNAL_TWO);
+        return new ResponseEntity<>(HttpStatus.Series.SUCCESSFUL.name(), HttpStatus.OK);
+    }
+}
